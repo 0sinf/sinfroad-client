0dcea54fe52fae94b689f314d69ffd430b45f5ac
정세영 <wjdtp93@gmail.com>
2022-05-06 19:57:20 +0900

    Fix style detail page

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
+import "./PostDetail.css";
 import { PostRes } from "../types/post";
 import { Carousel } from "./Carousel";
 
@@ -10,13 +11,17 @@ export function PostDetail() {
   const getPost = useCallback(() => {
     if (response && response.data.images) {
       const images = response.data.images.map((image) => image.url);
-
+      const date = new Date(response.data.created).toLocaleString("ko-KR");
       return (
         <div className="post__detail">
           <Carousel images={images} />
-          <div className="post__detail-title">{response.data.title}</div>
-          <div className="post__detail-created">{response.data.created}</div>
-          <div className="post__detail-contents">{response.data.contents}</div>
+          <div className="post__detail-box">
+            <div className="post__detail-title">{response.data.title}</div>
+            <div className="post__detail-created">{date}</div>
+            <div className="post__detail-contents">
+              {response.data.contents}
+            </div>
+          </div>
         </div>
       );
     }
