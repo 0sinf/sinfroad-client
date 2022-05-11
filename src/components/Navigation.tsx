1182c34c9542755837c050bb849656696fe4d0c6
정세영 <wjdtp93@gmail.com>
2022-05-11 18:08:50 +0900

    Add Navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+export function Navigation() {
+  return (
+    <>
+      <nav className="nav">
+        <Link to="/" className="nav__items">
+          Sinfroad
+        </Link>
+      </nav>
+    </>
+  );
+}
