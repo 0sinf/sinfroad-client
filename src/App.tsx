095e0d3df007b607f960941876fe0c75cab43de4
정세영 <wjdtp93@gmail.com>
2022-05-11 17:01:53 +0900

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Navigation } from "./components/Navigation";
-import { PostDetail } from "./components/PostDetail";
-import { PostList } from "./components/PostList";
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
