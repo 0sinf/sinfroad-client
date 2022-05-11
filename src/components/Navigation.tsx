0e4437cb0a6a9d8330e17e70e798abb5ec7eacac
정세영 <wjdtp93@gmail.com>
2022-05-11 19:27:18 +0900

    Fix background-color style

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,12 +3,10 @@ import "./Navigation.css";
 
 export function Navigation() {
   return (
-    <>
-      <nav className="nav">
-        <Link to="/" className="nav__items">
-          Sinfroad
-        </Link>
-      </nav>
-    </>
+    <nav className="nav">
+      <Link to="/" className="nav__items">
+        Sinfroad
+      </Link>
+    </nav>
   );
 }
