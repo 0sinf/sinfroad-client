97074eebbfae2769a9c395289d26b8d3afe79dc2
정세영 <wjdtp93@gmail.com>
2022-05-12 11:46:39 +0900

    Add posts fetcher

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,26 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function Home() {
-  useEffect(() => {});
+  const [posts, setPosts] = useState<PostInList[]>();
 
-  return <></>;
+  const getPosts = useCallback(() => {
+    if (!posts) {
+      console.log("Loading data...💾");
+      return;
+    }
+  }, [posts]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const res = await fetch("http://localhost:3001/api/posts", {
+        method: "GET",
+      });
+      const data = await res.json();
+      setPosts(data.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  return <>{getPosts()}</>;
 }
