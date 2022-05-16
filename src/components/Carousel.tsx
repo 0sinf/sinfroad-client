1c7e551dae42e7ddacf1d73f374b8bc3624f5273
정세영 <wjdtp93@gmail.com>
2022-05-16 20:16:20 +0900

    Add carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,31 +2,46 @@ import { useState, useRef, useEffect } from "react";
 import "./Carousel.css";
 
 export default function Carousel({ images }: { images: string[] }) {
-  const container = useRef<HTMLDivElement>(null);
-
   const [index, setIndex] = useState<number>(0);
   const [containerWidth, setContainerWidth] = useState<number>(0);
+  const container = useRef<HTMLDivElement>(null);
 
   function handleClick(direction: "prev" | "next") {
-    console.log(direction);
+    if (direction === "next") {
+      return setIndex((x) => (x < images.length - 1 ? x + 1 : 0));
+    }
+    return setIndex((x) => (x > 0 ? x - 1 : images.length - 1));
   }
 
-  useEffect(() => {
+  //TODO: 캐러셀 사이즈 리사이징 문제로 절반만 넘어가는 버그
+  const handleResize = () => {
     const { current } = container;
 
     if (!current) {
       return;
     }
 
-    setContainerWidth(images.length * 100);
+    setContainerWidth(current.offsetWidth);
+  };
+
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener("resize", handleResize, { passive: true });
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
-    <div className="carousel">
+    <div className="carousel" ref={container}>
       <div
         className="carousel__slider"
-        ref={container}
-        style={{ width: `${containerWidth}%` }}
+        style={{
+          width: `${images.length}00%`,
+          transform: `translate3d(${index * -containerWidth}px, 0, 0)`,
+        }}
       >
         {images.map((image, idx) => (
           <div className="carousel__items" key={idx}>
@@ -35,18 +50,18 @@ export default function Carousel({ images }: { images: string[] }) {
             </figure>
           </div>
         ))}
-        <button
-          className="carousel__button carousel__button-left"
-          aria-label="이전"
-          onClick={() => handleClick("prev")}
-        />
-
-        <button
-          className="carousel__button carousel__button-right"
-          aria-label="다음"
-          onClick={() => handleClick("next")}
-        />
       </div>
+      <button
+        className="carousel__button carousel__button-left"
+        aria-label="이전"
+        onClick={() => handleClick("prev")}
+      />
+
+      <button
+        className="carousel__button carousel__button-right"
+        aria-label="다음"
+        onClick={() => handleClick("next")}
+      />
     </div>
   );
 }
