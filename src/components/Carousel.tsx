481cf3e2e94e11d191b26b08ef2fd09adf98845e
정세영 <wjdtp93@gmail.com>
2022-05-16 19:53:11 +0900

    Fix carousel width

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,9 @@ import "./Carousel.css";
 
 export default function Carousel({ images }: { images: string[] }) {
   const container = useRef<HTMLDivElement>(null);
+
   const [index, setIndex] = useState<number>(0);
+  const [containerWidth, setContainerWidth] = useState<number>(0);
 
   function handleClick(direction: "prev" | "next") {
     console.log(direction);
@@ -15,29 +17,36 @@ export default function Carousel({ images }: { images: string[] }) {
     if (!current) {
       return;
     }
+
+    setContainerWidth(images.length * 100);
   }, []);
 
   return (
-    <div className="carousel non-scroll" ref={container}>
-      {images.map((image, idx) => (
-        <div className="carousel__items" key={idx}>
-          <figure className="carousel__item">
-            <img src={image} />
-          </figure>
-        </div>
-      ))}
+    <div className="carousel">
+      <div
+        className="carousel__slider"
+        ref={container}
+        style={{ width: `${containerWidth}%` }}
+      >
+        {images.map((image, idx) => (
+          <div className="carousel__items" key={idx}>
+            <figure className="carousel__item">
+              <img src={image} />
+            </figure>
+          </div>
+        ))}
+        <button
+          className="carousel__button carousel__button-left"
+          aria-label="이전"
+          onClick={() => handleClick("prev")}
+        />
 
-      <button
-        className="carousel__button carousel__button-left"
-        aria-label="이전"
-        onClick={() => handleClick("prev")}
-      />
-
-      <button
-        className="carousel__button carousel__button-right"
-        aria-label="다음"
-        onClick={() => handleClick("next")}
-      />
+        <button
+          className="carousel__button carousel__button-right"
+          aria-label="다음"
+          onClick={() => handleClick("next")}
+        />
+      </div>
     </div>
   );
 }
