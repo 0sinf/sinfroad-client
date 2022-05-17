357146c2474147758a7ea1fd284fe56278627666
정세영 <wjdtp93@gmail.com>
2022-05-17 14:04:11 +0900

    Add infinite scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,52 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 import "./Home.css";
 import { PostCard } from "../components/PostCard";
 
 export function Home() {
-  const [posts, setPosts] = useState<PostInList[]>();
+  const [page, setPage] = useState<number>(1);
+  const [posts, setPosts] = useState<PostInList[]>([]);
 
-  const getPosts = useCallback(() => {
-    if (!posts) {
-      return;
-    }
+  const loader = useRef<HTMLDivElement>(null);
 
-    return posts.map((post, idx) => {
-      return <PostCard key={idx} post={post} />;
+  async function fetchPosts() {
+    const res = await fetch(`http://localhost:3001/api/posts?page=${page}`, {
+      method: "GET",
     });
-  }, [posts]);
+    const data = await res.json();
+    setPosts((prev) => [...prev, ...data.posts]);
+  }
 
   useEffect(() => {
-    async function fetchPosts() {
-      const res = await fetch("http://localhost:3001/api/posts", {
-        method: "GET",
-      });
-      const data = await res.json();
-      setPosts(data.posts);
-    }
-
     fetchPosts();
+  }, [page]);
+
+  const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
+    const target = entries[0];
+
+    if (target.isIntersecting) {
+      setPage((prev) => prev + 1);
+    }
   }, []);
 
-  return <main className="main">{getPosts()}</main>;
+  useEffect(() => {
+    const option: IntersectionObserverInit = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) {
+      observer.observe(loader.current);
+    }
+  }, [handleObserver]);
+
+  return (
+    <main className="main">
+      {posts.map((post, idx) => {
+        return <PostCard key={idx} post={post} />;
+      })}
+      <div ref={loader} />
+    </main>
+  );
 }
