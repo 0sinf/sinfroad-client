2a04f90813ea713d82069e21018cf879dedb7a1e
정세영 <wjdtp93@gmail.com>
2022-05-26 10:58:53 +0900

    feat: Add detele post

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect, useCallback } from "react";
 import { Post } from "../components/Post";
 import "./PostDetail.css";
@@ -6,6 +6,8 @@ import "./PostDetail.css";
 export function PostDetail() {
   const [post, setPost] = useState<Post>();
   const { id } = useParams();
+
+  const go = useNavigate();
 
   const getPost = useCallback(() => {
     if (!post) {
@@ -30,6 +32,28 @@ export function PostDetail() {
     fetchPost();
   }, []);
 
+  async function handleClick() {
+    // TODO: confirm modal
+
+    const response = await fetch(
+      `${import.meta.env.VITE_API_SERVER_URI}/posts/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      // TODO: toast message
+      return;
+    }
+
+    // TODO: success message
+    go("/");
+  }
+
   return (
     <main className="main">
       <div className="post__control">
@@ -38,6 +62,8 @@ export function PostDetail() {
             수정하기
           </Link>
         </button>
+
+        <button onClick={handleClick}>삭제하기</button>
       </div>
       {getPost()}
     </main>
