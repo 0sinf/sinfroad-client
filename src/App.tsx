ef62c151f7a553758af44154882ee0e5f9255205
정세영 <wjdtp93@gmail.com>
2022-06-03 01:01:26 +0900

    style: Fix path css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 import { Navigation } from "./components/Navigation";
 import { Home } from "./pages/Home";
 import Login from "./pages/Login";
