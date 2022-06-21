73b6b0a9cbcc8107061ea2f82f1dd2a9bd7c5b95
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-21 13:47:33 +0900

    chore: Init project (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import "./css/App.css";
-import { Navigation } from "./components/Navigation";
-import { Home } from "./pages/Home";
-import Login from "./pages/Login";
-import { PostDetail } from "./pages/PostDetail";
-import PostForm from "./pages/PostForm";
-
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/posts/:id" element={<PostDetail />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/posts" element={<PostForm />}></Route>
-        <Route path="/posts/update/:id" element={<PostForm />} />
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
