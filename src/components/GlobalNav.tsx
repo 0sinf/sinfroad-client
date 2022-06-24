7da7dd44ac189688162aafbfe2219dabc0067314
정세영 <wjdtp93@gmail.com>
2022-06-24 09:37:15 +0900

    style: Add CSS in nav

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -2,9 +2,11 @@ import "./GlobalNav.css";
 
 export default function GlobalNav() {
   return (
-    <div className="nav__items">
-      <div className="nav__item">Sinfroad</div>
-      <div className="nav__item">Login</div>
+    <div className="nav">
+      <div className="nav__items">
+        <div className="nav__item">Sinfroad</div>
+        <div className="nav__item">Login</div>
+      </div>
     </div>
   );
 }
