8a596b96f2eb051313756dab3f503ded152a8d49
정세영 <wjdtp93@gmail.com>
2022-06-24 09:19:51 +0900

    chore: Add Global Nav

---
+++ src/components/GlobalNav.tsx
@@ -0,0 +1,10 @@
+import "./GlobalNav.css";
+
+export default function GlobalNav() {
+  return (
+    <div className="nav__items">
+      <div className="nav__item">Sinfroad</div>
+      <div className="nav__item">Login</div>
+    </div>
+  );
+}
