a0144a8091ec96f16c525e0286f560ca1c53ad5c
정세영 <wjdtp93@gmail.com>
2022-06-25 09:03:20 +0900

    style: Fix loading message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -53,7 +53,7 @@ export function Home() {
             return <PostCard key={idx} post={post} />;
           })
         ) : (
-          <p>"Dev..."</p>
+          <p>"Loading..."</p>
         )}
       </div>
       <div ref={loader} />
