5f6ca9b2f3395a14db843c164f4ad0ba4c9be185
정세영 <wjdtp93@gmail.com>
2022-06-28 13:48:13 +0900

    feat: Add Detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import GlobalNav from "./components/GlobalNav";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <GlobalNav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/posts/:id" element={<Detail />} />
       </Routes>
     </div>
   );
