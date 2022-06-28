dd96821023e1a3fa4c19e5a9d963dcfa09e56c3d
정세영 <wjdtp93@gmail.com>
2022-06-28 12:40:44 +0900

    feat: Get posts by fetch

---
+++ src/@types/posts.d.ts
@@ -0,0 +1,9 @@
+export interface Post {
+  id: string;
+  title: string;
+  contents: string;
+  address: string;
+  images: string[];
+  created: string;
+  updated: string;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,32 @@
-export default function Home() {}
+import { useEffect, useState } from "react";
+import { Post } from "../@types/posts";
+
+export default function Home() {
+  // TODO: Write Post card
+
+  const [posts, setPosts] = useState<Array<Post>>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getPosts = async () => {
+    const data = await (
+      await fetch(`${import.meta.env.VITE_API_SERVER_URI}/posts`)
+    ).json();
+
+    setPosts(data.posts);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading ... </p>
+      ) : (
+        posts.map((post) => <div key={post.title}>{post.title}</div>)
+      )}
+    </>
+  );
+}
