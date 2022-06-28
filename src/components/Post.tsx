4b794ee2078390762fb12176c07f80b73a21ae9c
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-28 14:57:29 +0900

    feat: Add Post component (#25)
    * style: Fix class name in postcard
    
    * feat: Add Post component
    
    * design: Add post style
    
    * design: Fix padding in post style
    
    * design: Fix style for date, address in post

---
+++ src/components/Post.tsx
@@ -0,0 +1,26 @@
+import { IPost } from "../@types/posts";
+
+import "./Post.css";
+
+export default function Post({
+  post: { title, contents, address, created, updated, images },
+}: {
+  post: IPost;
+}) {
+  const date = new Date(created).toLocaleDateString("ko-KR");
+
+  return (
+    <div className="post">
+      <figure>
+        {/* FIXME: Fix Carousel */}
+        <img className="post__img" src={images[0]} alt={title} />
+      </figure>
+      <article className="post__description">
+        <h1 className="post__title">{title}</h1>
+        <div className="post__date">{date}</div>
+        <div className="post__address">{address}</div>
+        <div className="post__contents">{contents}</div>
+      </article>
+    </div>
+  );
+}
