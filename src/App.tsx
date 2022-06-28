dc922a24ca33a7bb5a66e20d26e4726e571ba19f
정세영 <wjdtp93@gmail.com>
2022-06-28 12:32:47 +0900

    feat: Add route for Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import GlobalNav from "./components/GlobalNav";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <GlobalNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
