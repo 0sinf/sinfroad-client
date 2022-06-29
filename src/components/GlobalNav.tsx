f4819f0a24070d3c5c110489ea9ea2cdc81f603a
정세영 <wjdtp93@gmail.com>
2022-06-29 18:09:51 +0900

    feat: Add logout

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 import "./GlobalNav.css";
 import useAuthStore from "../store/useAuthStore";
+import deleteCookie from "../utils/delete-cookie";
 
 export default function GlobalNav() {
   const token = useAuthStore.getState().token;
+
+  const onDeleteUser = () => {
+    localStorage.removeItem("access-token");
+    deleteCookie();
+    window.location.reload();
+  };
 
   return (
     <div className="nav">
@@ -11,11 +18,13 @@ export default function GlobalNav() {
         <Link to="/" className="nav__item">
           Sinfroad
         </Link>
+        {/* // TODO: Use icon  */}
         {token ? (
-          ""
+          <Link to="/" className="nav__item" onClick={onDeleteUser}>
+            Logout
+          </Link>
         ) : (
           <Link to="/login" className="nav__item">
-            {/* // TODO: Use icon  */}
             Login
           </Link>
         )}
