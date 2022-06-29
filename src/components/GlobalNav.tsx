35b70e6add5a2cba979c4402deb18094a0c92b4e
정세영 <wjdtp93@gmail.com>
2022-06-29 17:58:39 +0900

    feat: Remove login when already logined

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
 import "./GlobalNav.css";
+import useAuthStore from "../store/useAuthStore";
 
 export default function GlobalNav() {
+  const token = useAuthStore.getState().token;
+
   return (
     <div className="nav">
       <div className="nav__items">
         <Link to="/" className="nav__item">
           Sinfroad
         </Link>
-        <Link to="/login" className="nav__item">
-          {/* // TODO: Use icon  */}
-          Login
-        </Link>
+        {token ? (
+          ""
+        ) : (
+          <Link to="/login" className="nav__item">
+            {/* // TODO: Use icon  */}
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
