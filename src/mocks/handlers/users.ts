54adfe44b937336b80635161dfc806115dfc01a1
정세영 <wjdtp93@gmail.com>
2022-06-30 09:36:33 +0900

    style: Fix url for getting user

--- src/mocks/handlers/users.ts
+++ src/mocks/handlers/users.ts
@@ -1,17 +1,14 @@
 import { rest } from "msw";
 
 export const users = [
-  rest.get(
-    `${import.meta.env.VITE_API_SERVER_URI}/users/:id`,
-    (_, res, ctx) => {
-      const user = {
-        id: "fad05c41-ead7-432e-8107-bbe783ddd3c9",
-        email: "email@email.com",
-        name: "name",
-        role: "ADMIN",
-      };
+  rest.get(`${import.meta.env.VITE_API_SERVER_URI}/users`, (_, res, ctx) => {
+    const user = {
+      id: "fad05c41-ead7-432e-8107-bbe783ddd3c9",
+      email: "email@email.com",
+      name: "name",
+      role: "ADMIN",
+    };
 
-      return res(ctx.status(200), ctx.json({ user }));
-    }
-  ),
+    return res(ctx.status(200), ctx.json({ user }));
+  }),
 ];
