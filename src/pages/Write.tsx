f2546204f8810cf18c608088239d2eb27955739d
정세영 <wjdtp93@gmail.com>
2022-06-30 14:04:43 +0900

    feat: Add create post page

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,1 +1,10 @@
-export default function Write() {}
+import "./Write.css";
+
+export default function Write() {
+  return (
+    <main className="write__container">
+      <h1>새 글 작성하기</h1>
+      {/* TODO: Add Input, Textarea, Button */}
+    </main>
+  );
+}
