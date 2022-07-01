e06a02cbc6095aa8233f58b7d7d2747a02dfa366
정세영 <wjdtp93@gmail.com>
2022-07-01 19:12:51 +0900

    feat: Fix showing nav items

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,10 +1,14 @@
+import { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./GlobalNav.css";
 import useAuthStore from "../store/useAuthStore";
 import deleteCookie from "../utils/delete-cookie";
+import "./GlobalNav.css";
 
 export default function GlobalNav() {
   const { user, setUser } = useAuthStore();
+
+  const [navWidth, setNavWidth] = useState<number>(0);
+  const nav = useRef<HTMLDivElement>(null);
 
   const onDeleteUser = () => {
     localStorage.removeItem("access-token");
@@ -13,22 +17,48 @@ export default function GlobalNav() {
     window.location.reload();
   };
 
+  const onResize = () => {
+    const { current } = nav;
+
+    if (!current) {
+      return;
+    }
+
+    setNavWidth(current.offsetWidth);
+  };
+
+  useEffect(() => {
+    onResize();
+
+    window.addEventListener("resize", onResize, { passive: true });
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className="nav" ref={nav}>
       <div className="nav__items">
         <Link to="/" className="nav__item--head">
           Sinfroad
         </Link>
-        <div className="nav__item">
-          {user?.role === "ADMIN" && <Link to="/posts">새 글 쓰기</Link>}
-          {user ? (
-            <Link to="/" onClick={onDeleteUser}>
-              Logout
-            </Link>
-          ) : (
-            <Link to="/login">Login</Link>
-          )}
-        </div>
+        {navWidth > 660 ? (
+          <div className="nav__item">
+            {user?.role === "ADMIN" && <Link to="/posts">새 글 쓰기</Link>}
+            {user ? (
+              <Link to="/" onClick={onDeleteUser}>
+                Logout
+              </Link>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+          </div>
+        ) : (
+          ""
+          // TODO: Add hamburger
+          // TODO: Add hidden window
+        )}
         {/* // TODO: Use icon  */}
       </div>
     </div>
