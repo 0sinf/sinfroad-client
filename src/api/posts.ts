a1bc37e14bfa6cebd455f0ce3309c2f82964e525
정세영 <wjdtp93@gmail.com>
2022-07-06 11:46:53 +0900

    feat: Add delete post

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -52,3 +52,17 @@ export async function updatePost(
 
   return response;
 }
+
+export async function deletePost(id: string, token?: string) {
+  const response = await fetch(
+    `${import.meta.env.VITE_API_SERVER_URI}/posts/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response;
+}
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FormEvent } from "react";
 import { IPost } from "../@types/posts";
 import Carousel from "./Carousel";
 import Button from "./Button";
+import useAuthStore from "../store/useAuthStore";
+import { deletePost } from "../api/posts";
 import "./Post.css";
 
 export default function Post({ post }: { post: IPost }) {
+  const { token } = useAuthStore();
+  const go = useNavigate();
+
   const { id, title, contents, address, created, updated, images } = post;
   const date = new Date(created).toLocaleDateString("ko-KR");
+
+  const handleDelete = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!confirm("정말 삭제하시겠습니까?")) {
+      return;
+    }
+
+    const response = await deletePost(id, token);
+
+    if (!response.ok) {
+      // TODO: Error
+      return;
+    }
+
+    go("/");
+  };
 
   return (
     <div className="post">
@@ -17,7 +40,14 @@ export default function Post({ post }: { post: IPost }) {
           <Link to={`/posts/update/${id}`} state={post}>
             <Button value="수정하기" size="small" />
           </Link>
-          <Button value="삭제하기" btnStyle="secondary" size="small" />
+          <form method="DETELE" onSubmit={handleDelete}>
+            <Button
+              type="submit"
+              value="삭제하기"
+              btnStyle="secondary"
+              size="small"
+            />
+          </form>
         </div>
         <h1 className="post__title">{title}</h1>
         <div className="post__date">{date}</div>
