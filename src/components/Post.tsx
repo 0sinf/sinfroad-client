2c76ac74ab1c9b285a4da1db295841231f261255
Young <43630508+young-sinf@users.noreply.github.com>
2022-07-06 11:01:40 +0900

    feat: Add update post (#44)
    * feat: Fix button for state
    
    * feat: Add router
    
    * feat: Add update-post
    
    * style: Fix mock

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,14 +1,11 @@
+import { Link } from "react-router-dom";
 import { IPost } from "../@types/posts";
 import Carousel from "./Carousel";
 import Button from "./Button";
-
 import "./Post.css";
 
-export default function Post({
-  post: { title, contents, address, created, updated, images },
-}: {
-  post: IPost;
-}) {
+export default function Post({ post }: { post: IPost }) {
+  const { id, title, contents, address, created, updated, images } = post;
   const date = new Date(created).toLocaleDateString("ko-KR");
 
   return (
@@ -17,7 +14,9 @@ export default function Post({
 
       <article className="post__description">
         <div className="post__control">
-          <Button value="수정하기" size="small" />
+          <Link to={`/posts/update/${id}`} state={post}>
+            <Button value="수정하기" size="small" />
+          </Link>
           <Button value="삭제하기" btnStyle="secondary" size="small" />
         </div>
         <h1 className="post__title">{title}</h1>
