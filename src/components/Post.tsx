c0768c03a0dc7e46ecc0184e84991d02c3f8171c
정세영 <wjdtp93@gmail.com>
2022-07-06 13:10:04 +0900

    fix: Add checking user for post-control

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -8,7 +8,7 @@ import { deletePost } from "../api/posts";
 import "./Post.css";
 
 export default function Post({ post }: { post: IPost }) {
-  const { token } = useAuthStore();
+  const { token, user } = useAuthStore();
   const go = useNavigate();
 
   const { id, title, contents, address, created, updated, images } = post;
@@ -36,19 +36,23 @@ export default function Post({ post }: { post: IPost }) {
       <Carousel title={title} images={images} />
 
       <article className="post__description">
-        <div className="post__control">
-          <Link to={`/posts/update/${id}`} state={post}>
-            <Button value="수정하기" size="small" />
-          </Link>
-          <form method="DETELE" onSubmit={handleDelete}>
-            <Button
-              type="submit"
-              value="삭제하기"
-              btnStyle="secondary"
-              size="small"
-            />
-          </form>
-        </div>
+        {user?.role === "ADMIN" ? (
+          <div className="post__control">
+            <Link to={`/posts/update/${id}`} state={post}>
+              <Button value="수정하기" size="small" />
+            </Link>
+            <form method="DETELE" onSubmit={handleDelete}>
+              <Button
+                type="submit"
+                value="삭제하기"
+                btnStyle="secondary"
+                size="small"
+              />
+            </form>
+          </div>
+        ) : (
+          ""
+        )}
         <h1 className="post__title">{title}</h1>
         <div className="post__date">{date}</div>
         <div className="post__address">{address}</div>
