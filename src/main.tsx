3cb0f9af6f3bd822e742958e1f16059b504fa7bf
정세영 <wjdtp93@gmail.com>
2022-07-20 12:23:07 +0900

    chore: Move index.css

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import useAuthStore from "./store/useAuthStore";
 
-import "./index.css";
+import "./css/index.css";
 
 async function prepare() {
   if (import.meta.env.DEV) {
