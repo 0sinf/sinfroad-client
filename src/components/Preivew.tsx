67a4ebe5167b9493d745bb33235cc793bea5ad2c
정세영 <wjdtp93@gmail.com>
2022-07-21 12:04:07 +0900

    chore: Add remove icon

--- src/components/Preivew.tsx
+++ src/components/Preivew.tsx
@@ -1,16 +1,20 @@
+import { XCircleFill } from "react-bootstrap-icons";
 import "./Preview.css";
 
 export default function Preview({ images }: { images: File[] }) {
   return (
     <div className="preview">
       {images.map((image) => (
-        <img
-          className="preview__image"
-          key={image.name}
-          src={URL.createObjectURL(image)}
-          alt={image.name}
-        />
+        <figure className="preview__container" key={image.name}>
+          <img
+            className="preview__image"
+            src={URL.createObjectURL(image)}
+            alt={image.name}
+          />
+          <XCircleFill className="preview__button" />
+        </figure>
       ))}
+      {/* TODO: Add remove button, and handleClick  */}
     </div>
   );
 }
