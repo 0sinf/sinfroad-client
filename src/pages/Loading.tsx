f1e6b892cf837ee7a9733e4f34d9c85636c3eca2
정세영 <wjdtp93@gmail.com>
2022-07-26 13:34:58 +0900

    feat: Add login page style

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,3 +1,9 @@
+import "./Loading.css";
+
 export default function Loading() {
-  return <p>{"loading"}</p>;
+  return (
+    <div className="loading__container">
+      <div className="scale-up-center loading__circle">loading</div>
+    </div>
+  );
 }
