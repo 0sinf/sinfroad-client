c2dc5cd3a583b87e69188a2f5f66232975797e07
정세영 <wjdtp93@gmail.com>
2022-08-18 13:03:16 +0900

    style: Remove hover when liked

--- src/components/Heart.tsx
+++ src/components/Heart.tsx
@@ -11,7 +11,7 @@ export function Heart({
     <svg
       width="20"
       height="20"
-      className={liked ? "heart heart--fill" : "heart"}
+      className={liked ? "heart heart--fill" : "heart heart--unfill"}
       onClick={handleClickLike}
     >
       <path d="M 10 7.55 L 7.5 4.925 L 5 4.925 L 2.5 7.55 L 2.5 10.175 L 2.5 11.75 L 5 14.375 L 7.5 17 L 10 19.625 L 12.5 17 L 15 14.375 L 17.5 11.75 L 17.5 10.175 L 17.5 7.55 L 15 4.925 L 12.5 4.925 L 10 7.55" />
