68f17b1291048cda32add92a89583a5a2fbbade9
정세영 <wjdtp93@gmail.com>
2022-08-18 12:49:01 +0900

    fix: Use custom heart

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import { FormEvent, useState } from "react";
-import { Heart, HeartFill } from "react-bootstrap-icons";
 import { IPost } from "../@types/posts";
 import Carousel from "./Carousel";
 import Button from "./Button";
@@ -13,10 +12,12 @@ export default function Post({ post }: { post: IPost }) {
   const { user } = useAuthStore();
   const go = useNavigate();
 
-  const { id, title, contents, address, created, images, beliked } = post;
+  const { id, title, contents, address, created, images, beliked, likes } =
+    post;
   const date = new Date(created).toLocaleDateString("ko-KR");
 
   const [liked, setLiked] = useState(beliked);
+  const [count, setCount] = useState(likes);
 
   const handleClickLike = async () => {
     if (liked) {
@@ -25,12 +26,16 @@ export default function Post({ post }: { post: IPost }) {
       if (!response.ok) {
         toast(data);
       }
+
+      setCount((prev) => prev - 1);
     } else {
       const { response, data } = await addLike(id);
 
       if (!response.ok) {
         toast(data);
       }
+
+      setCount((prev) => prev + 1);
     }
 
     setLiked((prev) => !prev);
@@ -77,12 +82,16 @@ export default function Post({ post }: { post: IPost }) {
         )}
         <h1 className="post__title">{title}</h1>
         <div className="post__date">{date}</div>
-        <div className="post__action" onClick={handleClickLike}>
-          {liked ? (
-            <HeartFill className="post__heart post__heart--fill" />
-          ) : (
-            <Heart className="post__heart" />
-          )}
+        <div className="post__action">
+          <svg
+            width="20"
+            height="20"
+            className={liked ? "post__heart post__heart--fill" : "post__heart"}
+            onClick={handleClickLike}
+          >
+            <path d="M 10 7.55 L 7.5 4.925 L 5 4.925 L 2.5 7.55 L 2.5 10.175 L 2.5 11.75 L 5 14.375 L 7.5 17 L 10 19.625 L 12.5 17 L 15 14.375 L 17.5 11.75 L 17.5 10.175 L 17.5 7.55 L 15 4.925 L 12.5 4.925 L 10 7.55" />
+          </svg>
+          <span>{count}</span>
         </div>
         <div className="post__address">{address}</div>
         <div className="post__contents">{contents}</div>
