17ea566c32e57724ba74c3d5e71ce1775c4b10f2
정세영 <wjdtp93@gmail.com>
2022-08-27 11:08:37 +0900

    style: Add comment form style

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,6 +1,7 @@
 import { useState, FormEvent } from "react";
 import { Input } from "./Input";
 import Button from "./Button";
+import "./Comment.css";
 
 export function Comment() {
   // TODO: Comment form
@@ -19,11 +20,18 @@ export function Comment() {
         type="text"
         name="comment"
         value={comment}
+        className="comment__input"
         placeholder="댓글을 입력해주세요! 👏🏻"
         setValue={setComment}
         withLabel={false}
       />
-      <Button value="작성" type="submit" disabled={false} />
+      <Button
+        value="작성"
+        type="submit"
+        disabled={false}
+        size="small"
+        className="comment__button"
+      />
     </form>
   );
 }
