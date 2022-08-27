04f6ecc33e0c77449a145085bcf7773fe01fac4c
정세영 <wjdtp93@gmail.com>
2022-08-27 11:55:04 +0900

    fix: Fix comment fetch

--- src/api/comments.ts
+++ src/api/comments.ts
@@ -1,14 +1,19 @@
 import fetcher from "../utils/fetcher";
 
 export async function createComment(postId: string, contents: string) {
+  const body = {
+    postId,
+    contents,
+  };
+
   const { response, data } = await fetcher(
     `${import.meta.env.VITE_API_SERVER_URI}/comments`,
     {
       method: "POST",
-      body: JSON.stringify({
-        postId,
-        contents,
-      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     }
   );
 
