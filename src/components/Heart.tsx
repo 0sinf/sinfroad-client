22676879ead5f13cb32d42df1d3b3859a0993729
정세영 <wjdtp93@gmail.com>
2022-09-03 09:54:53 +0900

    feat: Add message when click like without login

--- src/components/Heart.tsx
+++ src/components/Heart.tsx
@@ -1,4 +1,5 @@
 import "./Heart.css";
+import toast from "../utils/toast";
 
 export function Heart({
   liked,
@@ -14,7 +15,14 @@ export function Heart({
   return (
     <div>
       {disabled ? (
-        <svg width="30" height="20" className="heart--disabled">
+        <svg
+          width="30"
+          height="20"
+          className="heart--disabled"
+          onClick={() => {
+            toast("로그인이 필요한 서비스입니다.");
+          }}
+        >
           <path d="M 10 7.55 L 7.5 4.925 L 5 4.925 L 2.5 7.55 L 2.5 10.175 L 2.5 11.75 L 5 14.375 L 7.5 17 L 10 19.625 L 12.5 17 L 15 14.375 L 17.5 11.75 L 17.5 10.175 L 17.5 7.55 L 15 4.925 L 12.5 4.925 L 10 7.55" />
         </svg>
       ) : (
