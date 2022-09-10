f8a453490c67f8dc4ef8fb7616f30118eb440af3
Young <43630508+young-sinf@users.noreply.github.com>
2022-09-10 15:08:18 +0900

    feat: Implement updating username (#122)
    * chore: Add state for update
    
    * chore: Add button depending on update state
    
    * chore: Add to do and Add margin in button
    
    * chore: Manage state username
    
    * fix: Fix tag (div to form)
    
    * fix: Fix username state
    
    * feat: Update username
    
    * chore: When click cancel, return username

--- src/api/users.ts
+++ src/api/users.ts
@@ -7,3 +7,18 @@ export async function getUser() {
 
   return data;
 }
+
+export async function updateUsername(username: string) {
+  const { response, data } = await fetcher(
+    `${import.meta.env.VITE_API_SERVER_URI}/users`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+      body: JSON.stringify({ name: username }),
+    }
+  );
+
+  return { response, data };
+}
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,28 +1,80 @@
+import { FormEvent, useState, useEffect } from "react";
 import Button from "../components/Button";
 import { Input } from "../components/Input";
 import useAuthStore from "../store/useAuthStore";
+import Loading from "../components/Loading";
+import { updateUsername } from "../api/users";
+import toast from "../utils/toast";
 import "./User.css";
 
 export default function User() {
-  const { user } = useAuthStore();
+  // FIXME: when refresh page, Render error
+  const { user, setUser } = useAuthStore();
+
+  if (!user) {
+    return <Loading />;
+  }
+
+  const [isUpdating, setIsUpdating] = useState<boolean>(false);
+  const { name, email } = user;
+  const [username, setUsername] = useState<string>(name);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const { response } = await updateUsername(username);
+
+    if (!response.ok) {
+      toast("유저 정보를 수정할 수 없습니다.");
+    }
+
+    setUser({ ...user, name: username });
+    setIsUpdating(false);
+  };
 
   return (
     <main role="main" className="main">
-      <div className="user__container">
+      <form className="user__container" onSubmit={handleSubmit}>
         <Input
           name="email"
-          value={user?.email}
+          value={email}
           className="input user__email"
           disabled
         />
         <Input
           name="username"
-          value={user?.name}
+          value={username}
           className="input user__username"
-          disabled
+          setValue={setUsername}
+          disabled={!isUpdating}
         />
-        <Button value="수정하기" className="user__button-modify" />
-      </div>
+        {isUpdating ? (
+          <>
+            <Button
+              type="submit"
+              value="수정하기"
+              className="user__button-modify"
+            />
+            <Button
+              value="취소하기"
+              className="user__button-delete"
+              btnStyle="secondary"
+              onClick={() => {
+                setIsUpdating(false);
+                setUsername(user.name);
+              }}
+            />
+          </>
+        ) : (
+          <Button
+            value="수정하기"
+            className="user__button-modify"
+            onClick={() => {
+              setIsUpdating(true);
+            }}
+          />
+        )}
+      </form>
     </main>
   );
 }
