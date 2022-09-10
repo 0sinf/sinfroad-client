776811a9d5c4adecf8500a22ff96e96ec8f46801
Young <43630508+young-sinf@users.noreply.github.com>
2022-09-10 12:49:33 +0900

    feat: Add user page (#121)
    * chore: Add user page
    
    * design: Add style

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,3 +1,28 @@
+import Button from "../components/Button";
+import { Input } from "../components/Input";
+import useAuthStore from "../store/useAuthStore";
+import "./User.css";
+
 export default function User() {
-  return <div>user information</div>;
+  const { user } = useAuthStore();
+
+  return (
+    <main role="main" className="main">
+      <div className="user__container">
+        <Input
+          name="email"
+          value={user?.email}
+          className="input user__email"
+          disabled
+        />
+        <Input
+          name="username"
+          value={user?.name}
+          className="input user__username"
+          disabled
+        />
+        <Button value="수정하기" className="user__button-modify" />
+      </div>
+    </main>
+  );
 }
