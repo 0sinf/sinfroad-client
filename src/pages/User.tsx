f5f67903cfd381221dfd0f53386b0fa2b277d680
정세영 <wjdtp93@gmail.com>
2022-09-10 12:52:16 +0900

    chore: Add state for update

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -2,9 +2,11 @@ import Button from "../components/Button";
 import { Input } from "../components/Input";
 import useAuthStore from "../store/useAuthStore";
 import "./User.css";
+import { useState } from "react";
 
 export default function User() {
   const { user } = useAuthStore();
+  const [isUpdating, setIsUpdating] = useState<boolean>(false);
 
   return (
     <main role="main" className="main">
@@ -19,9 +21,15 @@ export default function User() {
           name="username"
           value={user?.name}
           className="input user__username"
-          disabled
+          disabled={!isUpdating}
         />
-        <Button value="수정하기" className="user__button-modify" />
+        <Button
+          value="수정하기"
+          className="user__button-modify"
+          onClick={() => {
+            setIsUpdating(() => true);
+          }}
+        />
       </div>
     </main>
   );
