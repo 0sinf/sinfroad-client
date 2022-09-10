2fab3212d1126cce679ad6cdddddb72ba4a3841d
정세영 <wjdtp93@gmail.com>
2022-09-10 12:48:09 +0900

    design: Add style

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,25 +1,28 @@
 import Button from "../components/Button";
 import { Input } from "../components/Input";
 import useAuthStore from "../store/useAuthStore";
+import "./User.css";
 
 export default function User() {
   const { user } = useAuthStore();
 
   return (
-    <div className="user__container">
-      <Input
-        name="email"
-        value={user?.email}
-        className="input user__email"
-        disabled
-      />
-      <Input
-        name="username"
-        value={user?.name}
-        className="input user__username"
-        disabled
-      />
-      <Button value="수정하기" className="user__button-modify" />
-    </div>
+    <main role="main" className="main">
+      <div className="user__container">
+        <Input
+          name="email"
+          value={user?.email}
+          className="input user__email"
+          disabled
+        />
+        <Input
+          name="username"
+          value={user?.name}
+          className="input user__username"
+          disabled
+        />
+        <Button value="수정하기" className="user__button-modify" />
+      </div>
+    </main>
   );
 }
