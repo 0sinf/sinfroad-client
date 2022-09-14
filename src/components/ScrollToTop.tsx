12ea01db0df5ae99291d8be45598925fedaf0f80
정세영 <wjdtp93@gmail.com>
2022-09-14 16:35:18 +0900

    feat: Add cover and reveal button

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,8 +1,10 @@
 import { ArrowUpCircleFill } from "react-bootstrap-icons";
+import { useEffect, useState } from "react";
 import "./ScrollToTop.css";
 
 export default function ScrollToTop() {
   const root = document.documentElement;
+  const [isTop, setIsTop] = useState<boolean>(true);
 
   const handleClick = () => {
     root.scrollTo({
@@ -11,5 +13,31 @@ export default function ScrollToTop() {
     });
   };
 
-  return <ArrowUpCircleFill className="scrollToTop" onClick={handleClick} />;
+  const handleScroll = () => {
+    if (window.scrollY < 400) {
+      setIsTop(true);
+      return;
+    }
+
+    setIsTop(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <ArrowUpCircleFill
+      className={
+        isTop
+          ? "scrollToTop scrollToTop-cover"
+          : "scrollToTop scrollToTop-reveal"
+      }
+      onClick={handleClick}
+    />
+  );
 }
