52bf2e21026a73c6ac583249a56135e5e0800a4f
정세영 <wjdtp93@gmail.com>
2022-09-14 15:43:57 +0900

    feat: Add scorll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,5 +2,14 @@ import { ArrowUpCircleFill } from "react-bootstrap-icons";
 import "./ScrollToTop.css";
 
 export default function ScrollToTop() {
-  return <ArrowUpCircleFill className="scrollToTop" />;
+  const handleClick = () => {
+    const root = document.documentElement;
+
+    root.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return <ArrowUpCircleFill className="scrollToTop" onClick={handleClick} />;
 }
